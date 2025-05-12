2a072510d81ff5e3c4f7f46fc37bf180ce630eab
Mariano <marianonovak@hotmail.com>
2025-05-12 20:19:24 -0300

    correccion final metodo cargar series

--- js/main.js
+++ js/main.js
@@ -10,35 +10,38 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("anterior").addEventListener("click", paginaAnterior);
 });
 
-async function cargarSeries(pagina) {
+function cargarSeries(pagina) {
   contenedorSeries.innerHTML = "";
 
   const inicio = (pagina - 1) * seriesPorPagina + 1;
 
+  function cargarUnaSerie(id) {
+    const url = `https://api.tvmaze.com/shows/${id}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(serieData => {
+        const serie = new Serie(
+          serieData.id,
+          serieData.url,
+          serieData.name,
+          serieData.language,
+          serieData.genres,
+          serieData.image?.medium
+        );
+
+        const htmlElement = serie.createHtmlElement();
+        contenedorSeries.appendChild(htmlElement);
+      })
+      .catch(error => {
+        console.error(`Error al cargar la serie con id ${id}:`, error);
+      });
+  }
+
   for (let i = inicio; i < inicio + seriesPorPagina; i++) {
-    const url = `https://api.tvmaze.com/shows/${i}`;
-
-    try {
-      const response = await fetch(url);
-      const serieData = await response.json();
-
-      const serie = new Serie(
-        serieData.id,
-        serieData.url,
-        serieData.name,
-        serieData.language,
-        serieData.genres,
-        serieData.image?.medium
-      );
-
-      const htmlElement = serie.createHtmlElement();
-      contenedorSeries.appendChild(htmlElement);
-
-    } catch (error) {
-      console.error(`Error al cargar la serie con id ${i}:`, error);
-    }
+    cargarUnaSerie(i);
   }
 }
+
 
 function paginaSiguiente() {
   paginaActual++;
